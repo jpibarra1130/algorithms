6332f9ec0cf6bf78c5e70eda0fc697a52c2e5891
Paolo Ibarra <jpibarra1130@gmail.com>
2015-09-03 22:47:02 +0800

    Added binary search implementation

---
+++ src/main/java/com/ibarra/search/BinarySearch.java
@@ -0,0 +1,47 @@
+package com.ibarra.search;
+
+import java.util.Scanner;
+
+/**
+ * Created by paoloibarra on 2/9/15.
+ */
+public class BinarySearch {
+
+    public int findIndex(int v, int[] array) {
+        int lo = 0;
+        int hi = array.length;
+
+        int mid = lo + (hi - lo) / 2;
+
+        while (lo < hi) {
+
+            if (array[mid] == v) {
+                return mid;
+            } else if (array[mid] < v) {
+                lo = mid++;
+                mid = lo + (hi - lo + 1) / 2;
+            } else if (array[mid] > v) {
+                hi = mid--;
+                mid = lo + (hi - lo) / 2;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int v = sc.nextInt();
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        BinarySearch binarySearch = new BinarySearch();
+        binarySearch.findIndex(v, array);
+    }
+
+}
